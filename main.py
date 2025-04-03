15cdebee6633719d0277de94a0ad5df10183b41e
lucy <lucyschonewille@gmail.com>
2025-04-03 22:16:57 +0200

    Added framerate limiter, added circleshape.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS,
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color=(0, 0, 0))
-        pygame.display.flip()
 
+        screen.fill(color=(0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
