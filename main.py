01713336ed2e24d19b3d98a1cbd1a83508292c51
lucy <lucyschonewille@gmail.com>
2025-04-04 18:19:17 +0200

    Added Player class that inherits from CircleShape and draws it on screen

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pygame
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS,
                        ASTEROID_KINDS, ASTEROID_SPAWN_RATE,
                        ASTEROID_MAX_RADIUS)
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                 return
 
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
